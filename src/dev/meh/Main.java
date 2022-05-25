a86c955c991fc2081e09ce211b21b0768e8227ef
memindeveci <devmeh.24@robcol.k12.tr>
2022-05-25 13:53:06 +0300

    init commit

--- src/dev/meh/Main.java
+++ src/dev/meh/Main.java
@@ -6,7 +6,6 @@ import dev.meh.Events.GreetPlayer;
 public class Main {
 
     public static void main(String[] args) {
-
         Game game = new Game();
         EventManager.getInstance().registerListener(new GreetPlayer());
 
